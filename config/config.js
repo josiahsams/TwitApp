bb585225471a67bd788a7f570f5e029168e10fb3
Josiah Samuel <josiahsams@gmail.com>
2018-03-10 15:39:43 +0530

    dockerize

--- config/config.js
+++ config/config.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
 
 module.exports = {
   consumer_key: 'R0s6DMzZZcTxwuq6VOU0BVN51',
-  consumer_secret: 'RnBcOUeKdVOLfmF89bB55X4d0eR70uS4i2p4A0DIHMB4WyBSHJ',
+  consumer_secret: process.env.CONSUMER_SECRECT,
   access_token: '969467272430538752-zgz9ZDxQTfnQJkZbGv12OYoKaK6ZaRT',
-  access_token_secret: 'ZzCIqEKX7hmRc8zTxlO9NBRqn4wF6fvFdSzg3ZyWsn6Dw',
+  access_token_secret: process.env.ACCESS_TOKEN_SECRECT,
   dburl: "mongodb://mongo:27017/joetest",
   predictor: "http://predictor:5000"
 }
